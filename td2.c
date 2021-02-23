1245e4d3977e76fda97089d6bcb27233163dd494
FADEL DIOP <64317234+fdael-art@users.noreply.github.com>
2021-02-23 12:49:55 +0000

    Add files via upload
    apprentissages des conditions if else

---
+++ td2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int jour;
+    printf("quel jourf sommes nous");
+
+        scanf("%d",&jour);
+
+    if(jour==1){
+        printf("nous sommes le lundi");
+    }else{
+    printf("le jour enntrer n est pas valider");}
+
+    return 0;
+}
