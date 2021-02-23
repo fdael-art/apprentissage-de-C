7a9e832c8b012080b4aa43162c8405936c4e40d8
FADEL DIOP <64317234+fdael-art@users.noreply.github.com>
2021-02-23 14:57:22 +0000

    Add files via upload

---
+++ condition-switch.c
@@ -0,0 +1,29 @@
+
+int main(void)
+{
+    int jour;
+    printf("veuillez entrez un numero de jour");
+
+        scanf("%d",&jour);
+
+   switch(jour){
+    case 1:
+        printf("ce numero de jour correspond a lundi");
+        break;
+    case 2:
+
+        printf("ce numero de jour correspond a mardi");
+        break;
+
+     case 3:
+
+        printf("ce numero de jour correspond a mercredie");
+        break;
+    default:("ce numero de jour n existe pas ");
+
+
+
+
+        }
+    return 0;
+}
